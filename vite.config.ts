8ae321b07463709b2911eec7bd0a20fd21ab49d9
haibkhn <hainguyen2000.bkhn@gmail.com>
2025-02-19 15:30:20 +0100

    try vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Add this line
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "UNUSED_EXTERNAL_IMPORT") return;
+        warn(warning);
+      },
+    },
+  },
 });
